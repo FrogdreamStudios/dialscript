6b762504fbea837513749c693b949a158ab41bbb
Arsenii <cubelius@icloud.com>
2025-12-05 17:57:48 +0100

    feat: dialog line wrapping with metadata support

--- compiler/verbose.c
+++ compiler/verbose.c
@@ -4,6 +4,10 @@
 
 #include "verbose.h"
 #include <stdio.h>
+#include <string.h>
+
+#define TERMINAL_WIDTH 96
+#define META_MAX_WIDTH 24
 
 void verbose_header(const char *fullpath) {
     printf("\033[1;36mCompiling:\033[0m %s\n", fullpath);
@@ -46,11 +50,51 @@ void verbose_characters(const int line_num, const char *val) {
 }
 
 void verbose_dialog_line(const int line_num, const char *name, const char *text, const char *meta) {
-    if (meta) {
-        printf("\033[90m%4d │   \033[1;37m%s:\033[0m %s \033[33m%s\033[0m\n",
-               line_num, name, text, meta);
+    const int name_len = (int)strlen(name);
+    const int max_text_width = TERMINAL_WIDTH - META_MAX_WIDTH - 4;
+    const int text_len = (int)strlen(text);
+
+    if (text_len <= max_text_width) {
+        printf("\033[90m%4d │   \033[1;37m%s:\033[0m %s", line_num, name, text);
+        for (int i = text_len; i < max_text_width; i++) printf(" ");
+        printf("    ");
+        if (meta) {
+            printf("\033[33m%s\033[0m", meta);
+        }
+        printf("\n");
     } else {
-        printf("\033[90m%4d │   \033[1;37m%s:\033[0m %s\n", line_num, name, text);
+        int wrap_pos = max_text_width;
+        while (wrap_pos > 0 && text[wrap_pos] != ' ') wrap_pos--;
+        if (wrap_pos == 0) wrap_pos = max_text_width;
+        printf("\033[90m%4d │   \033[1;37m%s:\033[0m %.*s", line_num, name, wrap_pos, text);
+        for (int i = wrap_pos; i < max_text_width; i++) printf(" ");
+        printf("    ");
+        if (meta) {
+            printf("\033[33m%s\033[0m", meta);
+        }
+        printf("\n");
+
+        const char *remaining = text + wrap_pos;
+        while (*remaining == ' ') remaining++;
+
+        while ((int)strlen(remaining) > 0) {
+            int chunk_len = (int)strlen(remaining);
+            if (chunk_len > max_text_width) {
+                int pos = max_text_width;
+                while (pos > 0 && remaining[pos] != ' ') pos--;
+                if (pos == 0) pos = max_text_width;
+                printf("\033[90m     │   ");
+                for (int i = 0; i < name_len + 2; i++) printf(" ");
+                printf("\033[0m%.*s\n", pos, remaining);
+                remaining += pos;
+                while (*remaining == ' ') remaining++;
+            } else {
+                printf("\033[90m     │   ");
+                for (int i = 0; i < name_len + 2; i++) printf(" ");
+                printf("\033[0m%s\n", remaining);
+                break;
+            }
+        }
     }
 }
 
